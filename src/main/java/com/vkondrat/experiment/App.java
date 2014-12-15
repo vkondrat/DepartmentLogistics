23ebcb82ae174e32ecb6bd3cb30a9e9c517fae03
Volodymyr Kondratenko <vkondrat87@gmail.com>
2014-12-14 18:20:07 -0700

    Reading & assigning classes from URL -not finished

--- src/main/java/com/vkondrat/experiment/App.java
+++ src/main/java/com/vkondrat/experiment/App.java
@@ -1,7 +1,9 @@
 package com.vkondrat.experiment;
 
 import com.google.gson.Gson;
+import com.vkondrat.experiment.entities.Department;
 import com.vkondrat.experiment.entities.Employee;
+import com.vkondrat.experiment.entities.Project;
 import com.vkondrat.experiment.util.JPAUtil;
 import spark.Request;
 import spark.Response;
@@ -18,10 +20,15 @@ import static spark.SparkBase.setPort;
 
 public class App 
 {
+    //////// Add it to the DB
+    //////// Add check for wrong input
+    //////// Figure out the right format for start and enddate
+
     public static void main( String[] args )throws SQLException {
 
         setPort(12345);
-        get("/dest", new Route() {
+
+        get("/test", new Route() {
             @Override
             public Object handle(Request request, Response response) {
                 Gson gson = new Gson();
@@ -34,6 +41,87 @@ public class App
                 return json;
             }
         });
+
+        get("/addEmployee", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                Gson gson = new Gson();
+                String data = request.queryParams("data");
+                Employee employee = gson.fromJson(data, Employee.class);
+                System.out.println(employee.toString());
+                String json = gson.toJson(employee);
+                return json;
+            }
+        });
+
+        get("/addDepartment", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                Gson gson = new Gson();
+                String data = request.queryParams("data");
+                Department department = gson.fromJson(data, Department.class);
+                System.out.println(department.toString());
+                String json = gson.toJson(department);
+                return json;
+            }
+        });
+        get("/addProject", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                Gson gson = new Gson();
+                String data = request.queryParams("data");
+                Project project = gson.fromJson(data, Project.class);
+                System.out.println(project.toString());
+                String json = gson.toJson(project);
+                return json;
+            }
+        });
+        get("/assignDepartmentToEmployee", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                Gson gson = new Gson();
+                String data = request.queryParams("data");
+                Employee employee = gson.fromJson(data, Employee.class);
+                System.out.println(employee.toString());
+                String json = gson.toJson(employee);
+                return json;
+            }
+        });
+        get("/assignEmployeesToDepartment", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                Gson gson = new Gson();
+                String data = request.queryParams("data");
+                Employee employee = gson.fromJson(data, Employee.class);
+                System.out.println(employee.toString());
+                String json = gson.toJson(employee);
+                return json;
+            }
+        });
+        get("/assignEmployeesToProject", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                Gson gson = new Gson();
+                String data = request.queryParams("data");
+                Employee employee = gson.fromJson(data, Employee.class);
+                System.out.println(employee.toString());
+                String json = gson.toJson(employee);
+                return json;
+            }
+        });
+        get("/assignProjectsToEmployee", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                Gson gson = new Gson();
+                String data = request.queryParams("data");
+                Employee employee = gson.fromJson(data, Employee.class);
+                System.out.println(employee.toString());
+                String json = gson.toJson(employee);
+                return json;
+            }
+        });
+
+
     }
 
 
