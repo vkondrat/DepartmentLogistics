047d2bdad93d9ec00576921c4750c27be9299b42
Volodymyr Kondratenko <vkondrat87@gmail.com>
2014-12-29 00:16:43 -0500

    Added POST methods
    Added POST methods to add an employee, a department and a project

--- src/main/java/com/vkondrat/experiment/MyServletPackage/RESTResources.java
+++ src/main/java/com/vkondrat/experiment/MyServletPackage/RESTResources.java
@@ -3,6 +3,7 @@ import com.google.gson.Gson;
 import com.sun.jersey.spi.container.servlet.PerSession;
 import com.vkondrat.experiment.entities.Department;
 import com.vkondrat.experiment.entities.Employee;
+import com.vkondrat.experiment.entities.Project;
 import com.vkondrat.experiment.service.CRUDService;
 import com.vkondrat.experiment.util.JPAUtil;
 
@@ -14,52 +15,41 @@ import java.sql.SQLException;
 import java.util.List;
 
 @PerSession
-@Path("/employees")
+@Path("/post")
 public class RESTResources {
 
     // Do only read and print first to check
 
 
     @POST
-    @Path("/post")
+    @Path("/employees")
     @Consumes("application/json")
 
     public void addEmployee(String jsonString) throws SQLException {
-
-        System.out.println(jsonString);
-        System.out.println("Is working!");
-
-        Gson gson = new Gson();
-        Employee entity = gson.fromJson(jsonString, Employee.class);
-        System.out.println(entity.toString());
-        System.out.println(gson.toJson(entity));
-        System.out.println(entity.getId());
-        System.out.println(entity.getName());
-        System.out.println(entity.getAge());
-
-        EntityManager em = JPAUtil.getInstance().getEm();
-
-        em.getTransaction().begin();
-        em.merge(entity);
-        em.getTransaction().commit();
-
-        Query query = em.createQuery("SELECT e FROM Employee e");
-
-        List<Employee> list = (List<Employee>) query.getResultList();
-        for (Employee employee : list) {
-            System.out.println(employee.getName());
-        }
-      //  em.getTransaction().begin();
-        em.close();
-        //   String test= new CRUDService<Employee>().saveFromJson(jsonString, Employee.class);
-        System.out.println("test2");
-
+        new CRUDService<Employee>().saveFromJson(jsonString, Employee.class);
     }
+
+    @POST
+    @Path("/departments")
+    @Consumes("application/json")
+
+    public void addDepartment(String jsonString) throws SQLException {
+        new CRUDService<Department>().saveFromJson(jsonString, Department.class);
+    }
+    @POST
+    @Path("/projects")
+    @Consumes("application/json")
+
+    public void addProject(String jsonString) throws SQLException {
+        new CRUDService<Project>().saveFromJson(jsonString, Project.class);
+    }
+
+
     // Testing
     @GET
-    @Path("{name}")
+    @Path("/test/{name}")
     public String sayHello(@PathParam("name") String name) throws SQLException{
-        StringBuilder stringBuilder = new StringBuilder("Check 20 Hello ");
+        StringBuilder stringBuilder = new StringBuilder("Check 21 Hello ");
         stringBuilder.append(name).append("!");
 
         return stringBuilder.toString();
